c01ed8bced73b74a62c3e972bbf8a5ed181a68c5
Sushant Gawali <sushantgawali@technogises-MacBook-Pro.local>
2026-02-26 12:02:35 +0530

    feat: enhance upload flow and add project documentation

--- src/app/pages/content/content.module.ts
+++ src/app/pages/content/content.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { ContentRoutingModule, routedComponents } from './content-routing.module';
 import { SharedModule } from '../shared/shared.module';
-import { ngfModule } from 'angular-file';
 import { QueryBuilderModule } from "angular2-query-builder";
 import { NbDialogModule } from '@nebular/theme';
 import { NgxDropzoneModule } from 'ngx-dropzone';
@@ -17,7 +16,6 @@ import { CustomModule } from '../custom-component/custom.module';
   imports: [
     ContentRoutingModule,
     SharedModule,
-    ngfModule,
     FileManagerModule,
     QueryBuilderModule,
     NbDialogModule.forChild(),
@@ -27,6 +25,6 @@ import { CustomModule } from '../custom-component/custom.module';
     MalihuScrollbarModule.forRoot(),
     CustomModule
   ],
-  exports: [ngfModule]
+  exports: []
 })
 export class ContentModule { }
--- src/app/pages/content/upload/upload.component.ts
+++ src/app/pages/content/upload/upload.component.ts
@@ -14,10 +14,18 @@ export class UploadComponent {
   @Input() onUpload = (files: File[]) => { };
 
   onFilesChange() {
-
     this.onUpload([...this.files]);
-
     this.files.length = 0;
+  }
+
+  onDropzoneChange(event: any) {
+    this.files.push(...event.addedFiles);
+    this.onUpload([...this.files]);
+    this.files.length = 0;
+  }
+
+  onRemove(file: File) {
+    this.files.splice(this.files.indexOf(file), 1);
   }
 
 }
