7028d845c69dd5ff58d4ae568fe5c027a06b93ad
Sushant Gawali <sushantgawali@technogises-MacBook-Pro.local>
2026-02-26 12:30:57 +0530

    test: stabilize options values list unit spec

--- src/app/pages/catalogue/options/options-values-list/options-values-list.component.spec.ts
+++ src/app/pages/catalogue/options/options-values-list/options-values-list.component.spec.ts
@@ -1,14 +1,52 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA, Pipe, PipeTransform } from '@angular/core';
+import { of } from 'rxjs';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { NbDialogService } from '@nebular/theme';
+import { ToastrService } from 'ngx-toastr';
 
 import { OptionsValuesListComponent } from './options-values-list.component';
+import { OptionValuesService } from '../services/option-values.service';
+import { StorageService } from '../../../shared/services/storage.service';
+import { StoreService } from '../../../store-management/services/store.service';
+
+@Pipe({ name: 'translate' })
+class TranslatePipeMock implements PipeTransform {
+  transform(value: string): string {
+    return value;
+  }
+}
 
 describe('OptionsValuesListComponent', () => {
   let component: OptionsValuesListComponent;
   let fixture: ComponentFixture<OptionsValuesListComponent>;
 
   beforeEach(waitForAsync(() => {
+    const optionValuesServiceMock = {
+      getListOfOptionValues: () => of({ recordsTotal: 0, optionValues: [] }),
+      deleteOptionValue: () => of({})
+    };
+    const storageServiceMock = {
+      getMerchant: () => 'DEFAULT',
+      getLanguage: () => 'en'
+    };
+    const storeServiceMock = {
+      getListOfMerchantStoreNames: () => of([])
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ OptionsValuesListComponent ]
+      declarations: [OptionsValuesListComponent, TranslatePipeMock],
+      providers: [
+        { provide: OptionValuesService, useValue: optionValuesServiceMock },
+        { provide: StorageService, useValue: storageServiceMock },
+        { provide: StoreService, useValue: storeServiceMock },
+        { provide: Router, useValue: { navigate: () => null } },
+        { provide: NbDialogService, useValue: { open: () => ({ onClose: of(false) }) } },
+        { provide: ToastrService, useValue: { success: () => null } },
+        { provide: TranslateService, useValue: { instant: (key: string) => key, onLangChange: of({}) } }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
