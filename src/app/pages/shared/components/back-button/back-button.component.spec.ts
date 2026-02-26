b5e025c203dad5b1f982dc11472c8cc5097b3f38
Sushant Gawali <sushantgawali@technogises-MacBook-Pro.local>
2026-02-26 12:35:48 +0530

    test: shallow-test back button component

--- src/app/pages/shared/components/back-button/back-button.component.spec.ts
+++ src/app/pages/shared/components/back-button/back-button.component.spec.ts
@@ -1,6 +1,15 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Location } from '@angular/common';
+import { NO_ERRORS_SCHEMA, Pipe, PipeTransform } from '@angular/core';
 
 import { BackButtonComponent } from './back-button.component';
+
+@Pipe({ name: 'translate' })
+class TranslatePipeMock implements PipeTransform {
+  transform(value: string): string {
+    return value;
+  }
+}
 
 describe('BackButtonComponent', () => {
   let component: BackButtonComponent;
@@ -8,7 +17,9 @@ describe('BackButtonComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ BackButtonComponent ]
+      declarations: [BackButtonComponent, TranslatePipeMock],
+      providers: [{ provide: Location, useValue: { back: () => null } }],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
